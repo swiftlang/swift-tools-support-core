fa483764557c8ccccbdfcf9c40dea1a1db359a7f
David Hart <david@hartbit.com>
2020-04-07 09:38:56 +0200

    Mark the current version as 5.3

--- Sources/TSCUtility/Versioning.swift
+++ Sources/TSCUtility/Versioning.swift
@@ -74,8 +74,8 @@ public struct Versioning {
 
     /// The current version of the package manager.
     public static let currentVersion = SwiftVersion(
-        version: (5, 2, 0),
-        isDevelopment: true,
+        version: (5, 3, 0),
+        isDevelopment: false,
         buildIdentifier: getBuildIdentifier())
 
     /// The list of version specific "keys" to search when attempting to load
