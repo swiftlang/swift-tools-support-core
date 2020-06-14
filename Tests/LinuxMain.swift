aac42e55e20756f8a2c0ce038a95a483bbf11994
Jeffrey Macko <github.jm@macko.fr>
2020-06-14 23:18:27 +0200

    Fix tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,1 +1,12 @@
-fatalError("Use `swift test --enable-test-discovery` to run tests")
+import XCTest
+
+import TSCBasicTests
+import TSCTestSupportTests
+import TSCUtilityTests
+
+var tests = [XCTestCaseEntry]()
+tests += TSCBasicTests.__allTests()
+tests += TSCTestSupportTests.__allTests()
+tests += TSCUtilityTests.__allTests()
+
+XCTMain(tests)
