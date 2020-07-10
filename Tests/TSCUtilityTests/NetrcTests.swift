6b76728c67e9bdc371f16269441dcc90ea4ce5c4
Stan Stadelman <sstadelman@gmail.com>
2020-07-10 16:22:07 -0700

    added netrc tests

--- Tests/TSCUtilityTests/NetrcTests.swift
+++ Tests/TSCUtilityTests/NetrcTests.swift
@@ -183,4 +183,56 @@ class NetrcTests: XCTestCase {
         }
         XCTAssertNil(netrc.authorization(for: URL(string: "https://example99.com")!))
     }
+    
+    func testIBMDocumentation() {
+        // test case: https://www.ibm.com/support/knowledgecenter/en/ssw_aix_72/filesreference/netrc.html
+        let content = "machine host1.austin.century.com login fred password bluebonnet"
+        
+        guard let netrc = try? Netrc.from(content).get() else {
+            return XCTFail()
+        }
+        
+        let machine = netrc.machines.first
+        XCTAssertEqual(machine?.name, "host1.austin.century.com")
+        XCTAssertEqual(machine?.login, "fred")
+        XCTAssertEqual(machine?.password, "bluebonnet")
+        
+    }
+    
+    func testNoErrorTrailingAccountMacdefDefault() {
+        // test case: https://gist.github.com/tpope/4247721
+        
+        // should not fail on presence of `account`, `macdef`, `default`
+        let content = """
+            machine api.heroku.com
+              login my@email.com
+              password 01230123012301230123012301230123
+
+            machine api.github.com password something login somebody
+
+            machine ftp.server login abc password def account ghi macdef somemacro
+            cd somehwhere
+            continues until end of paragraph
+
+            default login anonymous password my@email.com
+            """
+        
+        guard let netrc = try? Netrc.from(content).get() else {
+            return XCTFail()
+        }
+        
+        XCTAssertEqual(netrc.machines.count, 3)
+        
+        XCTAssertEqual(netrc.machines[0].name, "api.heroku.com")
+        XCTAssertEqual(netrc.machines[0].login, "my@email.com")
+        XCTAssertEqual(netrc.machines[0].password, "01230123012301230123012301230123")
+        
+        XCTAssertEqual(netrc.machines[1].name, "api.github.com")
+        XCTAssertEqual(netrc.machines[1].login, "somebody")
+        XCTAssertEqual(netrc.machines[1].password, "something")
+        
+        XCTAssertEqual(netrc.machines[2].name, "ftp.server")
+        XCTAssertEqual(netrc.machines[2].login, "abc")
+        XCTAssertEqual(netrc.machines[2].password, "def")
+    }
 }
