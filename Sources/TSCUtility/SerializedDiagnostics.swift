2434831f23bd900a87b9d9d38b4eb4990f120d8f
Owen Voorhees <owen@squibner.com>
2020-09-01 21:12:23 -0700

    Move LLVM bitstream utilities from swift-driver to TSC

---
+++ Sources/TSCUtility/SerializedDiagnostics.swift
@@ -0,0 +1,8 @@
+//
+//  SerializedDiagnostics.swift
+//  swift-tools-support-core
+//
+//  Created by Owen Voorhees on 9/1/20.
+//
+
+import Foundation
---
+++ Tests/TSCUtilityTests/SerializedDiagnosticsTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Owen Voorhees on 9/1/20.
+//
+
+import Foundation
