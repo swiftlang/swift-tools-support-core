c9ed8e4655355ef43c8dd33c8497368495bf6ca6
tom doron <tomer@apple.com>
2020-11-05 14:29:37 -0800

    add closable protocol
    motivation: marker protocol for entitites that manage resources and need to be closed to to cleanly free such resources
    
    changes:
    * define Closable protocol
    * conform WritableByteStrea to Closable
    * conform PersistenceCache to Closable

--- Sources/TSCUtility/PersistenceCache.swift
+++ Sources/TSCUtility/PersistenceCache.swift
@@ -19,7 +19,7 @@ public protocol PersistentCacheProtocol {
 }
 
 /// SQLite backed persistent cache.
-public final class SQLiteBackedPersistentCache: PersistentCacheProtocol {
+public final class SQLiteBackedPersistentCache: PersistentCacheProtocol, Closable {
     let db: SQLite
 
     init(db: SQLite) throws {
@@ -38,6 +38,10 @@ public final class SQLiteBackedPersistentCache: PersistentCacheProtocol {
 
     deinit {
         try? db.close()
+    }
+    
+    public func close() throws {
+        try db.close()
     }
 
     public convenience init(cacheFilePath: AbsolutePath) throws {
