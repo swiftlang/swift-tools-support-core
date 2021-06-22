f0e13a66e35b7e72d050acac9047e5ca4df89489
3405691582 <dsk@google.com>
2021-06-21 23:03:24 -0400

    Use the (versioned) triple from swiftc instead.
    Here, swiftc is invoked to get the textual unversioned triple from the
    compiler. However, TSC wants to use this unversioned triple for the
    target flag, which causes breakage on the compiler side on platforms
    with versioned triples, because the compiler can't find the stdlib with
    the unversioned triple.
    
    Instead, default to getting versioned triples. When clients of TSC want
    to do funky things with unversioned triples, we lop the version numbers
    off versioned triples to get an unversioned triple. This is somewhat
    crude, but the alternative is to get both versioned _and_ unversioned
    triples from swiftc, which is similarly awkward.

---
+++ Tests/TSCUtilityTests/TripleTests.swift
@@ -0,0 +1,28 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+import TSCUtility
+import XCTest
+
+class TripleTests : XCTestCase {
+    func testTriple() {
+        let linux = try? Triple("x86_64-unknown-linux-gnu")
+        XCTAssertNotNil(linux)
+        XCTAssertEqual(linux!.os, .linux)
+        XCTAssertNil(linux!.version)
+
+        let macos = try? Triple("x86_64-apple-macosx10.15")
+        XCTAssertNotNil(macos!)
+        XCTAssertEqual(macos!.version, "10.15")
+        let newVersion = "10.12"
+        let tripleString = macos!.tripleString(forPlatformVersion: newVersion)
+        XCTAssertEqual(tripleString, "x86_64-apple-macosx10.12")
+    }
+}
