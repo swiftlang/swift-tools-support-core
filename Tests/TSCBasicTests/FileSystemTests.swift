b214a12f61945c3bfd25d50735388a16387cd940
Saleem Abdulrasool <compnerd@compnerd.org>
2022-01-13 10:09:27 -0800

    Revert "FileLock encounters a runtime error when the path of the locked file is long (#277)"
    This reverts commit bccbd93e6d5f9d5ebbe689abc6b6ed6859594be5.

--- Tests/TSCBasicTests/FileSystemTests.swift
+++ Tests/TSCBasicTests/FileSystemTests.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+ Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See http://swift.org/LICENSE.txt for license information
@@ -767,12 +767,6 @@ class FileSystemTests: XCTestCase {
             let lockFile = tempDir.appending(component: "lockfile")
 
             try _testFileSystemFileLock(fileSystem: localFileSystem, fileA: fileA, fileB: fileB, lockFile: lockFile)
-
-            // Test some long and edge case paths. We arrange to split between the C and the Cedilla if NAME_MAX is 255.
-            let longEdgeCase1 = tempDir.appending(component: String(repeating: "Façade!  ", count: Int(NAME_MAX)).decomposedStringWithCanonicalMapping)
-            try localFileSystem.withLock(on: longEdgeCase1, type: .exclusive, {})
-            let longEdgeCase2 = tempDir.appending(component: String(repeating: "🏁", count: Int(NAME_MAX)).decomposedStringWithCanonicalMapping)
-            try localFileSystem.withLock(on: longEdgeCase2, type: .exclusive, {})
         }
     }
 
