a3d0116aac8ae0a7033a273dec812a46ac1c10a1
Alexey Ivashko <mcivashko@gmail.com>
2022-05-17 21:44:39 +0300

    Create helper functions for temporary directories which supports new concurrency features

--- Sources/TSCBasic/TemporaryFile.swift
+++ Sources/TSCBasic/TemporaryFile.swift
@@ -52,6 +52,7 @@ private extension TempFileError {
 ///     - dir: If present this will be the temporary directory.
 ///
 /// - Returns: Path to directory in which temporary file should be created.
+/// - Throws: `TempFileError`
 public func determineTempDirectory(_ dir: AbsolutePath? = nil) throws -> AbsolutePath {
     let tmpDir = dir ?? localFileSystem.tempDirectory
     guard localFileSystem.isDirectory(tmpDir) else {
@@ -231,22 +232,11 @@ extension MakeDirectoryError: CustomNSError {
 ///           return value for the `withTemporaryDirectory` function.
 ///           The cleanup block should be called when the temporary directory is no longer needed.
 ///
-/// - Throws: MakeDirectoryError and rethrows all errors from `body`.
+/// - Throws: `MakeDirectoryError` and rethrows all errors from `body`.
 public func withTemporaryDirectory<Result>(
     dir: AbsolutePath? = nil, prefix: String = "TemporaryDirectory" , _ body: (AbsolutePath, @escaping (AbsolutePath) -> Void) throws -> Result
 ) throws -> Result {
-    // Construct path to the temporary directory.
-    let templatePath = try AbsolutePath(prefix + ".XXXXXX", relativeTo: determineTempDirectory(dir))
-
-    // Convert templatePath to a C style string terminating with null char to be an valid input
-    // to mkdtemp method. The XXXXXX in this string will be replaced by a random string
-    // which will be the actual path to the temporary directory.
-    var template = [UInt8](templatePath.pathString.utf8).map({ Int8($0) }) + [Int8(0)]
-
-    if TSCLibc.mkdtemp(&template) == nil {
-        throw MakeDirectoryError(errno: errno)
-    }
-
+    let template = try createTemporaryDirectoryTemplate(dir: dir, prefix: prefix)
     return try body(AbsolutePath(String(cString: template))) { path in
         _ = try? FileManager.default.removeItem(atPath: path.pathString)
     }
@@ -267,7 +257,7 @@ public func withTemporaryDirectory<Result>(
 ///             If `body` has a return value, that value is also used as the
 ///             return value for the `withTemporaryDirectory` function.
 ///
-/// - Throws: MakeDirectoryError and rethrows all errors from `body`.
+/// - Throws: `MakeDirectoryError` and rethrows all errors from `body`.
 public func withTemporaryDirectory<Result>(
     dir: AbsolutePath? = nil, prefix: String = "TemporaryDirectory", removeTreeOnDeinit: Bool = false , _ body: (AbsolutePath) throws -> Result
 ) throws -> Result {
@@ -277,3 +267,70 @@ public func withTemporaryDirectory<Result>(
     }
 }
 
+/// Creates a temporary directory and evaluates a closure with the directory path as an argument.
+/// The temporary directory will live on disk while the closure is evaluated and will be deleted when
+/// the cleanup closure is called. This allows the temporary directory to have an arbitrary lifetime.
+///
+/// This function is basically a wrapper over posix's mkdtemp() function.
+///
+/// - Parameters:
+///     - dir: If specified the temporary directory will be created in this directory otherwise environment
+///            variables TMPDIR, TEMP and TMP will be checked for a value (in that order). If none of the env
+///            variables are set, dir will be set to `/tmp/`.
+///     - prefix: The prefix to the temporary file name.
+///     - body: A closure to execute that receives the absolute path of the directory as an argument.
+///           If `body` has a return value, that value is also used as the
+///           return value for the `withTemporaryDirectory` function.
+///           The cleanup block should be called when the temporary directory is no longer needed.
+///
+/// - Throws: `MakeDirectoryError` and rethrows all errors from `body`.
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public func withTemporaryDirectory<Result>(
+	dir: AbsolutePath? = nil, prefix: String = "TemporaryDirectory" , _ body: (AbsolutePath, @escaping (AbsolutePath) -> Void) async throws -> Result
+) async throws -> Result {
+    let template = try createTemporaryDirectoryTemplate(dir: dir, prefix: prefix)
+	return try await body(AbsolutePath(String(cString: template))) { path in
+		_ = try? FileManager.default.removeItem(atPath: path.pathString)
+	}
+}
+
+/// Creates a temporary directory and evaluates a closure with the directory path as an argument.
+/// The temporary directory will live on disk while the closure is evaluated and will be deleted afterwards.
+///
+/// This function is basically a wrapper over posix's mkdtemp() function.
+///
+/// - Parameters:
+///     - dir: If specified the temporary directory will be created in this directory otherwise environment
+///            variables TMPDIR, TEMP and TMP will be checked for a value (in that order). If none of the env
+///            variables are set, dir will be set to `/tmp/`.
+///     - prefix: The prefix to the temporary file name.
+///     - removeTreeOnDeinit: If enabled try to delete the whole directory tree otherwise remove only if its empty.
+///     - body: A closure to execute that receives the absolute path of the directory as an argument.
+///             If `body` has a return value, that value is also used as the
+///             return value for the `withTemporaryDirectory` function.
+///
+/// - Throws: MakeDirectoryError and rethrows all errors from `body`.
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public func withTemporaryDirectory<Result>(
+	dir: AbsolutePath? = nil, prefix: String = "TemporaryDirectory", removeTreeOnDeinit: Bool = false , _ body: (AbsolutePath) async throws -> Result
+) async throws -> Result {
+	try await withTemporaryDirectory(dir: dir, prefix: prefix) { path, cleanup in
+		defer { if removeTreeOnDeinit { cleanup(path) } }
+		return try await body(path)
+	}
+}
+
+private func createTemporaryDirectoryTemplate(dir: AbsolutePath?, prefix: String) throws -> [Int8] {
+    // Construct path to the temporary directory.
+    let templatePath = try AbsolutePath(prefix + ".XXXXXX", relativeTo: determineTempDirectory(dir))
+
+    // Convert templatePath to a C style string terminating with null char to be an valid input
+    // to mkdtemp method. The XXXXXX in this string will be replaced by a random string
+    // which will be the actual path to the temporary directory.
+    var template = [UInt8](templatePath.pathString.utf8).map({ Int8($0) }) + [Int8(0)]
+
+    if TSCLibc.mkdtemp(&template) == nil {
+        throw MakeDirectoryError(errno: errno)
+    }
+    return template
+}
