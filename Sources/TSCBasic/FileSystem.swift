22c4a91c30f1414105192c61a8083546f0136fb5
Max Desiatov <m_desiatov@apple.com>
2023-04-21 00:12:11 +0100

    Add `hasQuarantineAttribute` to `FileSystem` (#411)
    This new function returns `true` if a given path has a quarantine attribute applied if when file system supports this attribute, and returns `false` if such attribute is not applied or it isn't supported.

--- Sources/TSCBasic/FileSystem.swift
+++ Sources/TSCBasic/FileSystem.swift
@@ -168,6 +168,10 @@ public protocol FileSystem: Sendable {
     /// Check whether the given path is accessible and writable.
     func isWritable(_ path: AbsolutePath) -> Bool
 
+    /// Returns `true` if a given path has a quarantine attribute applied if when file system supports this attribute.
+    /// Returns `false` if such attribute is not applied or it isn't supported.
+    func hasQuarantineAttribute(_ path: AbsolutePath) -> Bool
+
     // FIXME: Actual file system interfaces will allow more efficient access to
     // more data than just the name here.
     //
@@ -293,6 +297,8 @@ public extension FileSystem {
     func withLock<T>(on path: AbsolutePath, type: FileLock.LockType, _ body: () throws -> T) throws -> T {
         throw FileSystemError(.unsupported, path)
     }
+
+    func hasQuarantineAttribute(_ path: AbsolutePath) -> Bool { false }
 }
 
 /// Concrete FileSystem implementation which communicates with the local file system.
@@ -339,6 +345,16 @@ private struct LocalFileSystem: FileSystem {
     func getFileInfo(_ path: AbsolutePath) throws -> FileInfo {
         let attrs = try FileManager.default.attributesOfItem(atPath: path.pathString)
         return FileInfo(attrs)
+    }
+
+    func hasQuarantineAttribute(_ path: AbsolutePath) -> Bool {
+#if canImport(Darwin)
+        let bufLength = getxattr(path.pathString, "com.apple.quarantine", nil, 0, 0, 0)
+
+        return bufLength > 0
+#else
+        return false
+#endif
     }
 
     var currentWorkingDirectory: AbsolutePath? {
