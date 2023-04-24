d809badf4a584b6a112b6ddda8174773caa926ea
Max Desiatov <m_desiatov@apple.com>
2023-04-24 19:05:42 +0100

    Update requirements in Package.swift to match SwiftPM
    We no longer support Swift 5.5 and older versions of macOS with SwiftPM, makes sense to bring TSC requirements in line with that.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 
 /*
  This source file is part of the Swift.org open source project
@@ -18,7 +18,7 @@ let macOSPlatform: SupportedPlatform
 if let deploymentTarget = ProcessInfo.processInfo.environment["SWIFTTSC_MACOS_DEPLOYMENT_TARGET"] {
     macOSPlatform = .macOS(deploymentTarget)
 } else {
-    macOSPlatform = .macOS(.v10_13)
+    macOSPlatform = .macOS("12.0")
 }
 
 let CMakeFiles = ["CMakeLists.txt"]
@@ -27,6 +27,7 @@ let package = Package(
     name: "swift-tools-support-core",
     platforms: [
         macOSPlatform,
+        .iOS("15.0")
     ],
     products: [
         .library(
