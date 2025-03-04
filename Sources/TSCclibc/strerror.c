fa40bf8acd97a7d5c012d6a468c00f28221ef41d
Rintaro Ishizaki <fs.output@gmail.com>
2025-03-03 17:11:34 -0800

    Revert "Use C to force the POSIX (not GNU) overload of strerror_r to be selected"
    This reverts commit baa8180c29a85535a0510b8c69fd433a27872c4b.

--- Sources/TSCclibc/strerror.c
+++
@@ -1,9 +0,0 @@
-#undef _GNU_SOURCE
-#include "strerror.h"
-#include <string.h>
-
-#ifndef _WIN32
-int tsc_strerror_r(int errnum, char *buf, size_t buflen) {
-    return strerror_r(errnum, buf, buflen);
-}
-#endif
