0a6a06f7cb86f2bf59d1792003659f6406635eea
Jake Petroules <jake.petroules@apple.com>
2025-06-10 12:58:19 -0700

    Fix the build on iOS due to missing availability annotations

--- Sources/TSCclibc/include/process.h
+++ Sources/TSCclibc/include/process.h
@@ -3,10 +3,24 @@
 #include <spawn.h>
 #include <stdbool.h>
 
+#ifdef TSC_API_UNAVAILABLE_DEFINED
+#error TSC_API_UNAVAILABLE_DEFINED already defined
+#endif
+
+#ifndef __API_UNAVAILABLE
+#define __API_UNAVAILABLE(...)
+#define TSC_API_UNAVAILABLE_DEFINED
+#endif
+
 // Wrapper method for posix_spawn_file_actions_addchdir_np that fails on Linux versions that do not have this method available.
-int SPM_posix_spawn_file_actions_addchdir_np(posix_spawn_file_actions_t *restrict file_actions, const char *restrict path);
+int SPM_posix_spawn_file_actions_addchdir_np(posix_spawn_file_actions_t *restrict file_actions, const char *restrict path) __API_UNAVAILABLE(ios, tvos, watchos, visionos);
 
 // Runtime check for the availability of posix_spawn_file_actions_addchdir_np. Returns 0 if the method is available, -1 if not.
 bool SPM_posix_spawn_file_actions_addchdir_np_supported();
+
+#ifdef TSC_API_UNAVAILABLE_DEFINED
+#undef TSC_API_UNAVAILABLE_DEFINED
+#undef __API_UNAVAILABLE
+#endif
 
 #endif
